b6d2e2fa0c15d4f22c41fafbda5e5a384b559b92
William Sundgård Anker <will1331@edu.sde.dk>
2022-08-19 11:09:53 +0200

    finished the BookingContactController.php

--- app/Http/Controllers/BookingContactController.php
+++ app/Http/Controllers/BookingContactController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Addon;
+use App\Models\Contact;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookingContactController extends Controller
 {
@@ -13,7 +17,15 @@ class BookingContactController extends Controller
      */
     public function index()
     {
-        //
+        if(!Auth::check())
+            return redirect()->intended('login');
+
+        $bookingscontactsPerPagination = 10;
+
+        $contact = Contact::paginate($bookingscontactsPerPagination);
+
+        return view('contact.index')
+            ->with('contact',$contact);
     }
 
     /**
@@ -40,45 +52,79 @@ class BookingContactController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(int $id)
     {
-        //
+        if(!Auth::check())
+            return redirect()->intended('login');
+
+        return view('contact.view')->with('contact',$id);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(int $id)
     {
-        //
+        if(!Auth::check())
+            return redirect()->intended('login');
+
+
+        return view('contact.view')->with('contact',$id);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id)
     {
-        //
+        if(!Auth::check())
+            return redirect()->intended('login');
+
+        $contact = Contact::find($id);
+        if(!$contact){
+            redirect()->route('contact.index');
+        }
+
+        $attr = $this->validate([
+            'fullname' => 'required|string|max:255',
+            'email' => 'required|String|max:255',
+            'phone' => 'required|int|digits:8'
+        ]);
+
+        $indput = $request->all();
+        $contact->update($indput);
+
+        return redirect()->route('contact.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(int $id)
     {
-        //
+        if(!Auth::check())
+            return redirect()->intended('login');
+
+        $contact = Contact::find($id);
+        if(!$contact)
+            redirect()->route('contact.index');
+
+        //Deletes a line in the specified point in the table.
+        $contact->delete();
+
+        return redirect()->route('contact.index');
     }
 }
